2775fa429ea302987432eaeda9cb901bd17f9301
hnmtri204 <120084560+hnmtri204@users.noreply.github.com>
2023-09-27 08:23:03 +0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+var firstName = "Phan Van";
+var lastName = "Nam";
+var old = 15;
+var markMath = 9;
+var passed = true;
+document.writeln("Ho va ten:" );
+document.writeln(firstName + " " + lastName +"<br>");
+document.writeln("Mark Math: " + markMath+"<br>");
+document.writeln("Passed: " + passed+"<br>");
+const PI = 3.14;
+document.writeln("Hằng số của Pi là: " + PI+"<br>");
+
